3206716d0b57b566daf13a74b7197738785f1197
Richard Teammco <teammcr192@gmail.com>
2015-04-03 16:54:15 -0500

    Updated template to make the whole Gauss map a template object.
    It now correctly assigns 2D angle bins.

--- viewer/decomposition/inverse_gauss_map.h
+++ viewer/decomposition/inverse_gauss_map.h
@@ -1,4 +1,11 @@
 /**
+ * The InverseGaussMap provides binning of arbitrary size (this value can be
+ * changed) that groups angles defined by normal vectors in 2D or 3D. This
+ * object provides constant-time lookups and resolution updates to the Gauss
+ * map, and only needs the 2D or 3D normal vector to return a mapping to the
+ * circle or sphere.
+ *
+ * The spherical Gauss map is based on Lambert mapping.
  */
 
 #ifndef INVERSE_GAUSS_MAP_H
@@ -6,31 +13,106 @@
 
 
 #include <assert.h>
+#include <math.h>
+
 #include "../../opencl/vec_cpp.h"
 
+#include <iostream>
 
-class InverseGaussMap {
+
+
+/** class InverseGaussMap
+ * Defines the inverse Gauss map object with constant-time bin functions and
+ * updatable resolution. The dimension of this object must be specified (2
+ * or 3 dimensions only). If D is invalid, the object will throw an assertion
+ * error during construction.
+ */
+template<int D> class InverseGaussMap
+{
 
   private:
-    int dimension;
-    int resolution;
 
-    int getBin2D(float x, float y);
-    int getBin3D(float x, float y, float z);
-    int getBinFromAngle(float angle, int k);
+    // The resolution
+    int resolution;
+    int resolution_sqrt;
+
+    /**
+     * Returns the bin number for a 2D circular Gauss map.
+     */
+    int getBin2D(float x, float y)
+    {
+        assert(D == 2);
+        float theta = acos(x);
+        if(y < 0)
+            theta += M_PI;
+        std::cout << "Angle: " << theta << std::endl;
+        return getBinFromAngle(theta, resolution);
+    }
+
+    /**
+     * Returns the bin number for a 3D spherical Gauss map.
+     */
+    int getBin3D(float x, float y, float z)
+    {
+        assert(D == 3);
+        float theta = 0;
+        float lambda = 0;
+        int x_bin = getBinFromAngle(theta, resolution_sqrt);
+        int y_bin = getBinFromAngle(lambda, resolution_sqrt);
+        return (y_bin * resolution_sqrt) + x_bin;
+    }
+
+    /**
+     * Returns the integer binning of the angle in between 0 and k (exclusive),
+     * provided the angle is between 0 and 2 Pi. If angle is larger than 2 Pi
+     * or smaller than 0, the resulting bin will be converted to the correct
+     * value. Assumes k is correctly larger than 0.
+     */
+    int getBinFromAngle(float theta, int k)
+    {
+        int bin = (int)floor((theta / (2*M_PI)) * k);
+        bin = bin % k;
+        if(bin < 0)
+            bin += 4;
+        return bin;
+    }
 
 
   public:
-    InverseGaussMap(int dimension, int resolution);
-
-    void setResolution(int resolution);
 
     /**
-     * Returns the bin for the correct dimension.
+     * Initialize the Gauss map in 2D or 3D (circle or sphere, respectively),
+     * and set up the initial resolution. Dimension must be 2 or 3, and
+     * resolution must be greater than 0.
      */
-    template<int D> int getBin(MyVec<float, D> normal) {
-        assert(D == dimension);
-        if(dimension == 2)
+    InverseGaussMap(int resolution)
+    {
+        assert(D == 2 || D == 3);
+        setResolution(resolution);
+    }
+
+    /**
+     * Set the resolution of the Gauss map. Resolution must be greater than 0.
+     * In 3D, resolution must be a perfect square, and will be set to the next
+     * largest perfect square if such a value is not provided.
+     */
+    void setResolution(int resolution)
+    {
+        assert(resolution > 0);
+        if(D == 3)
+        {
+            resolution_sqrt = (int)ceil(pow(resolution, 0.5));
+            resolution = resolution_sqrt * resolution_sqrt;
+        }
+        this->resolution = resolution;
+    }
+
+    /**
+     * Returns the bin for the correct dimension by calling the appropriate
+     * 2D or 3D bin function.
+     */
+    int getBin(MyVec<float, D> normal) {
+        if(D == 2)
             return getBin2D(normal.x, normal.y);
         else
             return getBin3D(normal.x, normal.y, normal.z);
--- viewer/decomposition/test.cpp
+++ viewer/decomposition/test.cpp
@@ -7,17 +7,17 @@ using namespace std;
 
 int main()
 {
-    InverseGaussMap g1(2, 8);
-    InverseGaussMap g2(3, 8);
+    InverseGaussMap<2> g1(4);
+    InverseGaussMap<3> g2(4);
 
     float2 x;
-    x.x = 2;
-    x.y = 1;
-    cout << g1.getBin<2>(x) << endl;
+    x.x = 1;
+    x.y = 0;
+    cout << g1.getBin(x) << endl;
 
     float3 y;
     y.x = 3;
     y.y = 2;
     y.z = 0;
-    cout << g2.getBin<3>(y) << endl;
+    cout << g2.getBin(y) << endl;
 }
